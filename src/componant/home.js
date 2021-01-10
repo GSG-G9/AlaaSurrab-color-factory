84106d7ba9b2fab7a3bf85c55715f53596451a0d
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-01-10 14:56:48 +0200

    basic stracture for the home route

--- src/componant/home.js
+++ src/componant/home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const home = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Welcome to the color factory </h1>
+        <a href="#">Add a color</a>
+      </div>
+      <div>
+        <h4>please select a color.</h4>
+        <ul></ul>
+      </div>
+    </>
+  );
 };
 
 export default home;
